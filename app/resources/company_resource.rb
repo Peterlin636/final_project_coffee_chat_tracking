da38050ecfd4dfce7dd33a553651dd7161fea1ec
anonymous anonymous <peter.lin@kellogg.northwestern.edu>
2022-03-15 16:46:31 +0000

    Generate graphiti resources for companies

---
+++ app/resources/company_resource.rb
@@ -0,0 +1,12 @@
+class CompanyResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :industry, :string
+  attribute :website, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
