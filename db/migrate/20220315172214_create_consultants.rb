b28efe76a530ffdeb24071738e4331c16ccdb6ac
anonymous anonymous <peter.lin@kellogg.northwestern.edu>
2022-03-15 17:23:35 +0000

    2nd draft

--- db/migrate/20220315172214_create_consultants.rb
+++ db/migrate/20220315172214_create_consultants.rb
@@ -2,8 +2,10 @@ class CreateConsultants < ActiveRecord::Migration[6.0]
   def change
     create_table :consultants do |t|
       t.integer :company_id
+      t.string :name
       t.string :title
       t.string :office
+      t.string :email
       t.string :phone_number
 
       t.timestamps
